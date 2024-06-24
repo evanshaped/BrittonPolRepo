dc8b1592fefa569d8f22fb130663c634db1a1c30
EvanShaped <2020nave@gmail.com>
2024-06-24 11:18:57 -0400

    moved read_pax_data from Dataset.py to dataframe_management_utils.py

--- data_manager/dataset.py
+++ data_manager/dataset.py
@@ -31,7 +31,7 @@ class Dataset:
         
         # Cleaning of the data happens in Dataset.read_pax_data
         df, num_points_dropped, device_id, serial_num, wavelength, basic_sample_rate, op_mode_period, \
-            op_mode_FFT_num = Dataset.read_pax_data(filename,set_range, skip_default_signal_baseline)
+            op_mode_FFT_num = dataframe_management_utils.read_pax_data(filename,set_range, skip_default_signal_baseline)
         self.device_id = device_id
         self.serial_num = serial_num
         self.wavelength = wavelength
@@ -51,128 +51,6 @@ class Dataset:
         self.avg_sample_rate = 1/(df['TimeDiff'][1:].mean())   # See rate_hist for why this code
         return
     
-    
-    
-    # skip_default_signal_baseline = True iff the first 10 seconds (no more than 20) are used only to identify
-    # which signal is the default one. These seconds will be automatically cut out. A value of t seconds can also
-    # be assigned, to give a custom number of seconds to skip (e.g. 100 if the first 100 seconds contains just one signal)
-    @staticmethod
-    def read_pax_data(filename, set_range=None, skip_default_signal_baseline=True):
-        # Get one-off information in the header
-        df_temp = pd.read_csv(filepath_or_buffer=filename, delimiter=';', header=None, usecols=[1], nrows=7)
-        device_id = df_temp.iat[0,0]
-        serial_num = df_temp.iat[1,0]
-        wavelength = float(df_temp.iat[4,0])
-        basic_sample_rate = float(df_temp.iat[5,0])
-        op_mode = df_temp.iat[6,0] #operating mode string
-        p_end_index = op_mode.find(' rev')
-        n_start_index = op_mode.find('ent, ') + 5
-        n_end_index = op_mode.find(' poi')
-        op_mode_period = float(op_mode[:p_end_index]) #P part of string
-        op_mode_FFT_num = int(op_mode[n_start_index:n_end_index]) #N part of string
-        # File looks like 7 rows of info (do display(df.head())), 1 row of whitespace, 1 row of column names, then the data
-        
-        ### We begin to take care of the range considerations. set_range can either be:
-        # None, (time_start,time_end), (per_start,per_end)
-        # If None, we use the whole dataset
-        # If (time_start,time_end), we read in the entire dataset and then chop off the seconds we want (this
-        # is usually useful to get rid of the first/last 10 minutes)
-        # If (per_start,per_end), we'll only read in from per_start percentage of the dataset to per_end (these
-        # parameters must be given between 0.0 and 1.0) (this is usually useful to save time while reading in
-        # a small portion of a large dataset). For example, (0.05, 0.1) means the chunk of the dataset between 5%-10%
-        with open(filename) as f:
-            total_rows = sum(1 for line in f) - 9   # The first 9 rows are info, whitespace, and column names
-        time_range = None   # Initially assume we don't want to cut off the times
-        skiprows = nrows = None   # Initally assume we read all of the file
-        if set_range is not None:
-            per_start, per_end = set_range
-            if ((per_start is not None) and (per_start>0) and (per_start<1)) or \
-                ((per_end is not None) and (per_end>0) and (per_end<1)):
-                #print('percentages detected')
-                # set_range parameters refer to percentages (e.g. (0.4,0.6) is 40% to 60% of the dataset)
-                # If per_start isn't None, skiprows is a number. If per_end isn't None, nrows is rows to use
-                skiprows = None if per_start is None else int(total_rows * per_start)
-                nrows = None if per_end is None else int(total_rows * (per_end-(0.0 if per_start is None else per_start)))
-                # If either of these were changed from None, read_csv will notice and use them appropriately
-            else:
-                #print('times detected')
-                time_range = set_range   # O/w, time_range is in seconds; this will be cut further on
-                set_range = None
-        # Coming out of this, either both time_range and set_range are None (no chopping necessary),
-        # or time_range is None but set_range has percentage start and ends (use in read_csv via skiprows and nrows),
-        # or set_range is None but time_range has time start and ends (cut further on)
-        
-        # TODO: fix warning
-        # Catching a FutureWarning for infer_datetime_formet;
-        # the code works and I don't want to deal with the red text right now
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore", FutureWarning)
-            ### read in, assign labels and variable types
-            df = pd.read_csv(filepath_or_buffer=filename, delimiter=';', header=7, skiprows=skiprows, nrows=nrows,
-                             parse_dates=['Timestamp'], infer_datetime_format=True,
-                             names=['Timestamp', 'TimeDelta', 's1', 's2', 's3', 'S0', 'S1', 'S2', 'S3',
-                                    'Azimuth', 'Ellipticity', 'DOP', 'DOCP', 'DOLP', 'Power', 'Power_pol', 'Power_unpol',
-                                    'Power_total_dB', 'Power_pol_dB', 'Power_unpol_dB', 'Power_split_ratio',
-                                    'Phase_difference', 'Warning'],
-                             dtype={'Warning': 'string'})
-
-        # drop unneeded columns
-        df.drop(columns=['Power_total_dB', 'Power_pol_dB', 'Power_unpol_dB', 'Warning'],inplace=True)
-
-        indices_with_missing_values = df[df.isna().any(axis=1)].index # Find the indices of rows with missing values
-        df = df.drop(indices_with_missing_values) # Drop rows with missing values
-        dfd = df.reset_index(drop=True) # Re-index the DataFrame
-        num_points_dropped = len(indices_with_missing_values)
-
-        # format timestamp
-        def custom_to_timedelta(s):
-            days, time = s.split('.')
-            hours, minutes, seconds, milliseconds = map(int, time.split(':'))
-            return timedelta(days=int(days), hours=hours, minutes=minutes, seconds=seconds, milliseconds=milliseconds)
-        df['TimeDelta'] = df['TimeDelta'].apply(custom_to_timedelta) # convert to timedelta
-        df['TimeElapsed'] = df['TimeDelta'].apply(lambda x: x.total_seconds()) # Seconds elapsed since first measurement
-        # The raw DateTime and TimeDelta objects are no longer needed
-        #df.drop(columns=['Timestamp','TimeDelta'], inplace=True)
-        df.drop(columns=['TimeDelta'], inplace=True)   # Actually, want to display global datetime on plots
-        
-        ### If desired, we only want to keep the range specified (time_range is given in seconds)
-        # This is mutually exclusive with cutting based on percentages given in set_range (see above)
-        # We do not reset the timestamps to be 0.000s at the first entry, but we do reset the indices
-        if time_range is not None:
-            set_start, set_end = dataframe_management_utils.fill_in_range(time_range, df)   # automatically fills in missing range vals
-            # Throw warning if including out of range data
-            if set_end > df.loc[df.shape[0]-1, 'TimeElapsed']:
-                print('WARNING: data out of range specified')
-                print('set_end={:.2f} > {:.2f}'.format(set_end,df.loc[df.shape[0]-1, 'TimeElapsed']))
-            if set_start < df.loc[0, 'TimeElapsed']:
-                print('WARNING: data out of range specified')
-                print('set_start={:.2f} < {:.2f}'.format(set_start,df.loc[0,'TimeElapsed']))
-            df = df[(set_start<=df['TimeElapsed']) & (df['TimeElapsed']<=set_end)].reset_index(drop=True)
-
-        
-        ### Here, we also include functionality to automatically cut off the first 20 seconds of the dataset
-        # (if it is the *true* first 20 seconds; not the first 20 seconds of a 5%-10% dataset)
-        # If it is a 5%-10% dataset, the timestamps will start partway through the dataset (e.g. 2500-3500 seconds),
-        # and this will have no effect (as desired)
-        # This is optionally performed because I often use the first 10 seconds of every SwitchSet to solely
-        # measure the default signal (not switching between the two signals), so we can match which signal is which
-        # between the input and output SwitchSets or between the 1345 and 1560 output SwitchSets
-        if skip_default_signal_baseline is not False:
-            time_skip = 20 if skip_default_signal_baseline is True else skip_default_signal_baseline
-            df = df[time_skip<=df['TimeElapsed']].reset_index(drop=True)
-        
-        
-        ### Calculate the time difference between current and previous measurements
-        df['TimeDiff'] = df['TimeElapsed'].diff()
-        df.loc[0, 'TimeDiff'] = 0
-
-        # rearrange columns
-        cols = df.columns.tolist()
-        cols = cols[-2:] + cols[:-2] # move TimeElapsed and TimeDiff to front
-        df = df[cols]
-        
-        return df, num_points_dropped, device_id, serial_num, wavelength, basic_sample_rate, op_mode_period, \
-            op_mode_FFT_num
     
     ### Generate histogram of time differences
     def rate_hist(self, log=True, bins=50, xmax=None):
